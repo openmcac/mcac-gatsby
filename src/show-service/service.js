375c13ec155123af538429b0f32e7daba83b4f96
Amos Chan <ac@amoschan.com>
2020-04-02 16:32:16 -0400

    Update styles for service screen

--- src/show-service/service.js
+++ src/show-service/service.js
@@ -27,8 +27,8 @@ const Background = styled.div`
 `
 
 const Announcements = styled.section`
-  background-color: #ffffff;
-  background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23e2e8f0' fill-opacity='0.4'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+  background-color: #000;
+  background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23f7fafc' fill-opacity='0.4'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 `
 
 const Service = ({ service }) => {
@@ -39,9 +39,9 @@ const Service = ({ service }) => {
   }
 
   const announcementRenderers = {
-    paragraph: ({ children }) => <p className="inline">{children}</p>,
+    paragraph: ({ children }) => <p className="inline bg-white text-gray-900">{children}</p>,
     listItem: ({ children }) => <li className="ml-6">{children}</li>,
-    list: ({ children }) => <ul className="list-disc">{children}</ul>,
+    list: ({ children }) => <ul className="list-disc text-gray-900">{children}</ul>,
     link: props => <a {...props} className="border-gray-700 text-black" />,
   }
 
@@ -51,8 +51,8 @@ const Service = ({ service }) => {
         <Header className="max-w-6xl mx-auto">
           <div className="mx-auto text-white h-56 sm:h-64 md:h-128 xl:h-256 flex items-center">
             <div className="mx-auto">
-              <div className="font-semibold tracking-wide text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl sm:tracking-wider mb-3 md:mb-6 lg:mb-8">{service.name}</div>
-              <div className="text-center text-sm sm:text-base md:text-lg lg:text-xl"><Moment format="MMMM Do YYYY [at] h:mm a">{service.publishedAt}</Moment></div>
+              <div className="bg-white text-gray-900 px-2 font-semibold tracking-wide text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl sm:tracking-wider mb-3 md:mb-6 lg:mb-8">{service.name}</div>
+              <div className="text-center text-sm sm:text-base md:text-lg lg:text-xl"><Moment className="bg-white text-gray-900 px-2 py-1" format="MMMM Do YYYY [at] h:mm a">{service.publishedAt}</Moment></div>
             </div>
           </div>
         </Header>
@@ -66,10 +66,12 @@ const Service = ({ service }) => {
       <Announcements className="border-t border-gray-200 bg-black text-gray-800 pb-12">
         <div className="max-w-2xl mx-auto">
           <HeaderIcon className="absolute ml-4 -mt-12">🗓</HeaderIcon>
-          <h2 className="pt-16 ml-3 text-xl sm:text-3xl font-bold mb-6">Announcements</h2>
-          <ol className="list-decimal ml-8 mr-4 sm:text-lg">
+          <h2 className="pt-16 ml-3 text-xl sm:text-3xl font-bold mb-6">
+            <span className="bg-white text-gray-900 px-2 py-1">Announcements</span>
+          </h2>
+          <ol className="list-decimal ml-8 mr-4 sm:text-lg text-white">
             {service.announcements.map((announcement, index) => (
-              <li key={index} className="mb-4 sm:mb-6">
+              <li key={index} className="mb-4 sm:mb-6 bg-white px-2 pb-1">
                 <Markdown source={announcement.description} renderers={announcementRenderers} />
               </li>
             ))}
