804d4d5f100dbbc2752f290e319d02ae441f770c
Amos Chan <ac@amoschan.com>
2020-04-16 13:37:27 -0400

    Make header's darkening show past container

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -9,13 +9,6 @@ const LogoImage = styled.img`
   filter: invert(100%);
 `
 
-const Container = styled.div`
-  background: linear-gradient(
-    rgba(0, 0, 0, 0.25), 
-    rgba(0, 0, 0, 0.25)
-  )
-`
-
 const Header = () => {
   const [isMenuVisible, setMenuVisible] = useState(false)
   const onMenuClose = () => setMenuVisible(false)
@@ -25,45 +18,45 @@ const Header = () => {
   }
 
   return (
-    <Container>
-      <div className="container mx-auto max-w-3xl h-12 flex flex-row justify-between items-center px-2">
-        <Link className="border-0" to="/">
-          <LogoImage src={logo} className="object-contain w-8" />
-        </Link>
-        <nav className="hidden md:block text-xs tracking-wider uppercase">
-          <ul>
-            <li className="inline-block text-white ml-6">
-              <Link to="/sunday" className="hover:text-white hover:opacity-75 border-0">
-                Sundays at 9:30am
-              </Link>
-            </li>
-            <li className="inline-block text-white ml-6">
-              <Link to="/english-service" className="hover:text-white hover:opacity-75 border-0">
-                News
-              </Link>
-            </li>
-            <li className="inline-block text-white ml-6">
-              <a href="http://giving.mcac.church" className="hover:text-gray-900 border border-solid px-4 py-1 hover:bg-white hover:text-gray-900">
-                Giving
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <MenuIcon className="block md:hidden text-3xl text-white" onClick={() => setMenuVisible(true)} />
-        <Drawer
-          title={<span className="tracking-widest uppercase">Menu</span>}
-          placement="top"
-          visible={isMenuVisible}
-          onClose={onMenuClose}
-        >
-          <ul className="text-xl">
-            <li><Link to="/english-service">News</Link></li>
-            <li><Link to="/sunday">Sundays</Link></li>
-            <li><a href="http://giving.mcac.church">Giving</a></li>
-          </ul>
-        </Drawer>
+    <div className="relative container mx-auto max-w-3xl h-12 flex flex-row justify-between items-center px-2">
+      <Link className="border-0" to="/">
+        <LogoImage src={logo} className="object-contain w-8" />
+      </Link>
+      <nav className="hidden md:block text-xs tracking-wider uppercase">
+        <ul>
+          <li className="inline-block text-white ml-6">
+            <Link to="/sunday" className="hover:text-white hover:opacity-75 border-0">
+              Sundays at 9:30am
+            </Link>
+          </li>
+          <li className="inline-block text-white ml-6">
+            <Link to="/english-service" className="hover:text-white hover:opacity-75 border-0">
+              News
+            </Link>
+          </li>
+          <li className="inline-block text-white ml-6">
+            <a href="http://giving.mcac.church" className="hover:text-gray-900 border border-solid px-4 py-1 hover:bg-white hover:text-gray-900">
+              Giving
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="md:hidden p-4 pr-0" onClick={() => setMenuVisible(true)}>
+        <MenuIcon className="block text-3xl text-white" />
       </div>
-    </Container>
+      <Drawer
+        title={<span className="tracking-widest uppercase">Menu</span>}
+        placement="top"
+        visible={isMenuVisible}
+        onClose={onMenuClose}
+      >
+        <ul className="text-xl">
+          <li><Link to="/english-service">News</Link></li>
+          <li><Link to="/sunday">Sundays</Link></li>
+          <li><a href="http://giving.mcac.church">Giving</a></li>
+        </ul>
+      </Drawer>
+    </div>
   )
 }
 
--- src/layouts/main.js
+++ src/layouts/main.js
@@ -2,15 +2,25 @@ import Footer from "./footer"
 import Header from "./header"
 import React from "react"
 import Show from "../common/show"
+import styled from "styled-components"
+
+const ShadedContainer = styled.div`
+  background: linear-gradient(
+    rgba(0, 0, 0, 0.25), 
+    rgba(0, 0, 0, 0.25)
+  )
+`
 
 const Main = ({ alert, children }) => {
   return (
     <div className="font-sans text-base text-gray-900">
       <div className="-mb-12">
-        <Show show={alert}>
-          {alert}
-        </Show>
-        <Header />
+        <ShadedContainer className="relative">
+          <Show show={alert}>
+            {alert}
+          </Show>
+          <Header />
+        </ShadedContainer>
       </div>
       {children}
       <Footer />
