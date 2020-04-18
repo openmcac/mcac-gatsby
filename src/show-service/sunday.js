d6bb634074f214f39e39a1e38dc9257c794860fd
Amos Chan <ac@amoschan.com>
2020-04-18 14:57:20 -0400

    Improve Livestream section

--- src/show-service/sunday.js
+++ src/show-service/sunday.js
@@ -2,9 +2,9 @@ import Loading from "../common/loading"
 import React from "react"
 import Service, { HeaderIcon } from "./service"
 import gql from "graphql-tag"
+import styled from "styled-components"
 import { Helmet } from "react-helmet"
 import { useQuery } from "@apollo/react-hooks"
-
 
 const FETCH_NEXT_SERVICE = gql`
   query sunday {
@@ -29,6 +29,24 @@ const FETCH_NEXT_SERVICE = gql`
   }
 `
 
+const Livestream = styled.section`
+  background-color: #e1e1e1;
+  background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.4'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+`
+
+const WidescreenAspectRatio = styled.div`
+ position: relative;
+ padding-top:56.25%;
+`
+
+const YouTubeFrame = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height:100%;
+`
+
 const Sunday = () => {
   const { loading, error, data } = useQuery(FETCH_NEXT_SERVICE)
 
@@ -38,24 +56,45 @@ const Sunday = () => {
 
   const { group } = data
 
+  const youtubeId = "Df_1jb6UDog"
+
   const livestream = (
-    <section className="pb-12 sm:pb-32 bg-black">
+    <Livestream className="pb-12 sm:pb-32 bg-black">
       <div className="max-w-2xl mx-auto">
         <HeaderIcon className="absolute ml-4 -mt-12">
           <span role="img" aria-label="Livestream icon">🎥</span>
         </HeaderIcon>
         <div className="text-center text-gray-600 pt-20">
-          <iframe
-            width="100%"
-            height="400"
-            src="https://www.youtube-nocookie.com/embed/xa2G56fGOhY"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen>
-          </iframe>
+          <div className="mb-2 md:mb-4">
+            <span className="md:text-xl py-1 px-2 text-gray-100 bg-gray-900">
+              Tune in <strong>LIVE</strong> on Sunday at 9:30 am
+            </span>
+          </div>
+          <WidescreenAspectRatio>
+            <YouTubeFrame
+              width="100%"
+              height="100%"
+              src={`https://www.youtube-nocookie.com/embed/${youtubeId}`}
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen>
+            </YouTubeFrame>
+          </WidescreenAspectRatio>
+          <div className="mt-6">
+            <div className="mb-2">
+              <span className="py-1 px-2 text-gray-100 bg-gray-900">
+                <a href={`https://www.youtube.com/watch?v=${youtubeId}`}>Open in YouTube</a>
+              </span>
+            </div>
+            <div className="mb-2">
+              <span className="py-1 px-2 text-gray-100 bg-gray-900">
+                <a href="https://fisherhall.mcac.church">Join us on Zoom after the service</a>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
+    </Livestream>
   )
 
   return (
