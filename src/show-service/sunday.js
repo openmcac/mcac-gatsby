8111867026f764a7ef61bc14e08711a815380591
Amos Chan <ac@amoschan.com>
2020-05-02 19:20:57 -0400

    Sermon Upload Take 2 🎬

--- src/show-service/sunday.js
+++ src/show-service/sunday.js
@@ -81,7 +81,7 @@ const Sunday = () => {
     setPaginationInfo(newBulletins.pageInfo)
   }
 
-  const youtubeId = "2lsvAtmGXOs"
+  const youtubeId = "7CERNafmTWQ"
 
   const livestream = (
     <Livestream className="pb-12 sm:pb-32 bg-black">
