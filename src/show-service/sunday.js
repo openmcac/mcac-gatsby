da5a980523da0d915437e9209c084e679812ea4b
Amos Chan <ac@amoschan.com>
2020-05-23 11:37:07 -0400

    YouTube update Take 2 🎬

--- src/show-service/sunday.js
+++ src/show-service/sunday.js
@@ -81,7 +81,7 @@ const Sunday = () => {
     setPaginationInfo(newBulletins.pageInfo)
   }
 
-  const youtubeId = "wIPEaTKRbh8"
+  const youtubeId = "PXpgWakQbTI"
 
   const livestream = (
     <Livestream className="pb-12 sm:pb-32 bg-black">
