892e0ed338aad61e08ae16f8b338a3c0f46c7b32
Amos Chan <ac@amoschan.com>
2020-07-05 01:41:25 -0400

    Oops

--- src/show-service/sunday.js
+++ src/show-service/sunday.js
@@ -81,7 +81,7 @@ const Sunday = () => {
     setPaginationInfo(newBulletins.pageInfo)
   }
 
-  const youtubeId = "EgB0dQPC1uE"
+  const youtubeId = "XWEduERGfeE"
 
   const livestream = (
     <Livestream className="pb-12 sm:pb-32 bg-black">
