947eb769cd6639e3623f6ac5b4acd8d0047d59d9
Amos Chan <ac@amoschan.com>
2020-12-26 09:54:33 -0500

    Revert "Update YouTube ID"
    This reverts commit a2643d3155ef63581c0a52373bf5e9fbe77ca86f.

--- src/show-service/sunday.js
+++ src/show-service/sunday.js
@@ -81,7 +81,7 @@ const Sunday = () => {
     setPaginationInfo(newBulletins.pageInfo)
   }
 
-  const youtubeId = "nN9OIhNkyOA"
+  const youtubeId = "OeGCkGFnAQA"
 
   const livestream = (
     <Livestream className="pb-12 sm:pb-32 bg-black">
