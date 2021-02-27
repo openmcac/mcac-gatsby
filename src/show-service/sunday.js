14616782ad6a6806c17b9fd5e130afb8aa84dd4e
Amos Chan <ac@amoschan.com>
2021-02-27 16:32:10 -0500

    Update YouTube ID

--- src/show-service/sunday.js
+++ src/show-service/sunday.js
@@ -81,7 +81,7 @@ const Sunday = () => {
     setPaginationInfo(newBulletins.pageInfo)
   }
 
-  const youtubeId = "oU8YuqLutW4"
+  const youtubeId = "-dCjNEdFnD0"
 
   const livestream = (
     <Livestream className="pb-12 sm:pb-32 bg-black">
